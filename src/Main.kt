94f7ed5f434fdf2a9593f29f85cad490fcf7201a
Alexei <titov_a@mail.ru>
2025-01-19 18:12:04 +0300

    Add for task1 fun palindrom (homework)

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello world!")
+    println("Lesson3 - Homework 1\n Введите число: ")
+    val num = readln()
+    palindrom(num)
 }
+
+// Домашнее задание №1. Проверить является ли число палиндромом
+fun palindrom(x : String) {
+    val str1 = x.toCharArray()
+    val str2 = str1.reversed()
+    var result = true
+    
+    for (i in 0..<str1.size) {
+        if (str1[i] != str2[i]) result = false
+    }
+
+    if (result) println("Число палиндром")
+    else println("Число не палиндром")
+}
+
